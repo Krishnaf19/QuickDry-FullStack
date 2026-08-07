e2fddb47a7ec3927e415e901be88de3d194c3ca9
Krishna Sonker <krishnasonker1919@gmail.com>
2026-08-07 23:10:39 +0530

    fix: update app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -45,7 +45,7 @@ app.use("/api/v1/order", orderRoute)
 app.use("/api/v1/payment", paymentRoute)
 
 // SPA 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"))
 })
 
