8724d493ac016f26bf4cc9736d1553ca5d0af496
Krishna Sonker <krishnasonker1919@gmail.com>
2026-08-07 17:14:28 +0530

    deploy correction

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ import { razorpayWebhook } from "./controllers/payment.controller.js"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "https://quickdry-frontend.onrender.com",
     credentials: true,
 }))
 
